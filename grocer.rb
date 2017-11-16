23c8afbf1c9b20e32a3775b6aad1fa3d9b665048
Matt Faircloth <mattcfaircloth@gmail.com>
2017-11-16 20:24:43 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,14 @@
 def consolidate_cart(cart)
-  # code here
+  cart.each_with_object({}) do |item_name, info|
+    item_name.each do |product, descriptors|
+      if info[product]
+        descriptors[:count] += 1
+      else
+        descriptors[:count] = 1
+        info[product] = descriptors
+      end
+    end
+  end
 end
 
 def apply_coupons(cart, coupons)
